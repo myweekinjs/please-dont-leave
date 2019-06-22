2bb491bae48ae76fdf6f3567db1a8e3c63b5b02e
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-06-22 16:32:11 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Gets the correct values based on browser
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API#Example
+ */
+const getBrowserHiddenProps = () => {
+  let hidden, visibilityChange
+  if (typeof document.hidden !== "undefined") {
+    hidden = "hidden";
+    visibilityChange = "visibilitychange";
+  } else if (typeof document.msHidden !== "undefined") {
+    hidden = "msHidden";
+    visibilityChange = "msvisibilitychange";
+  } else if (typeof document.webkitHidden !== "undefined") {
+    hidden = "webkitHidden";
+    visibilityChange = "webkitvisibilitychange";
+  }
+
+  return {
+    hidden,
+    visibilityChange
+  }
+}
+
 function App() {
+  const { hidden, visibilityChange } = getBrowserHiddenProps()
+
+  if (typeof document.addEventListener !== "undefined" && typeof hidden !== "undefined") {
+    document.addEventListener(visibilityChange, () => {
+      if (document[hidden]) {
+        document.title = "😭 PLEASE COME BACK!!"
+      } else {
+        document.title = "😍 YAY!"
+      }
+    }, false)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
